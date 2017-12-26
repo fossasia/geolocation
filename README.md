aed7ce96f24842ab59b4b8c7e7b1dabb05e931a7
Piyush Raj <contact@0x48piraj.co>
2017-12-26 12:45:45 +0530

    Bug Fix Test

---
+++ README.md
@@ -0,0 +1,17 @@
+# Geolocation finder
+This website is the official website of Fossasia when it comes to geolocations. It finds geolocation for integration in multiple projects 
+the community looks after. This is basically a mini-project of FOSSASIA.
+
+## Repositories currently using geolocation
+gci17.fossasia.org
+
+## How to use geolocation for other projects
+The website currently supports only the gci17.fossasia.org repository where you can add the geo co-ordinates for mentors and students, the
+members of FOSSASIA are free to supply any further integrations, check the code in the index.html and do the needy.
+
+## Branches:
+We follow a simple branch framework, currently the gh-pages branch that should'nt amaze you right?
+
+## Release History
+* 1.0
+    * CHANGE: Project born
---
+++ js/main.js
@@ -0,0 +1,49 @@
+var output = document.getElementById("output");
+var out = document.getElementById("out")
+function geolocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(rit,fileit);
+    }
+    else {
+        output.innerHTML = "Geolocation is not supported by this browser";
+        output.style.display = "inherit";
+    }
+}
+
+function rit(position) {
+    var mylocation = "lat: " + position.coords.latitude +"<br>";
+    mylocation += "lng: " + position.coords.longitude+"<br><br>";
+    output.innerHTML = mylocation;
+    my_preview = "https://www.google.com/maps/?q=" + position.coords.latitude + "," +position.coords.longitude;
+    preview.innerHTML = "<a target='_blank' href='"+my_preview+"'>PREVIEW on GMaps!</a>"+"<br><br>";
+    var element = document.getElementById('osm-map');
+    element.style = 'height:300px;';
+    var map = L.map(element);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    var target = L.latLng(position.coords.latitude, position.coords.longitude);
+    map.setView(target, 14);
+    L.marker(target).addTo(map);
+    output.style.display = "none";
+    output.style.display = "inherit";
+    out.style.display = "inherit";
+    document.getElementById("button").value="Get my geolocation again";
+}
+
+function fileit(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            output.innerHTML="Permission Denied, please give permission and try again";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            output.innerHTML="Position information is unavailable please try after some time";
+            break;
+        case error.TIMEOUT:
+            output.innerHTML ="Request Timed out, please try again";
+            break;
+        case error.UNKNOWN_ERROR:
+            output.innerHTML="Unknown Error please try again";
+            break;
+    }
+}
