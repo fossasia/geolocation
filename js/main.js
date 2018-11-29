4b62f38d6d5e8478066a2f9d8bf9011301b3bfeb
thakursachin467 <thakursachin467@gmail.com>
2018-11-30 00:45:04 +0530

    updated javaScript to ES6 Syntax

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var output = document.getElementById("output");
-var out = document.getElementById("out")
-function geolocation() {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(rit,fileit);
-        document.getElementById("button").value="Get my geolocation again";
+let output = document.getElementById("output");
+let out = document.getElementById("out")
+const geolocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(rit, fileit);
+        document.getElementById("button").value = "Get my geolocation again";
         document.getElementById("button").onclick = "window.location.reload();"; // reloads and executes the geolocation();
     }
     else {
@@ -12,19 +12,19 @@ function geolocation() {
     }
 }
 
-function rit(position) {
-    var mylocation = "lat: " + position.coords.latitude +"<br>";
-    mylocation += "lng: " + position.coords.longitude+"<br><br>";
+const rit = (position) => {
+    let mylocation = `lat:${position.coords.latitude}<br>`;
+    mylocation += `lng:${position.coords.longitude}<br><br>`;
     output.innerHTML = mylocation;
-    my_preview = "https://www.google.com/maps/?q=" + position.coords.latitude + "," +position.coords.longitude;
-    preview.innerHTML = "<a target='_blank' href='"+my_preview+"'>Google Maps!</a>"+"<br><br>";
-    var element = document.getElementById('osm-map');
+    my_preview = `https://www.google.com/maps/?q=${position.coords.latitude},${position.coords.longitude}`;
+    preview.innerHTML = `<a target='_blank' href='${my_preview}>Google Maps!</a><br><br>`;
+    let element = document.getElementById('osm-map');
     element.style = 'height:300px;';
-    var map = L.map(element);
+    let map = L.map(element);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-    var target = L.latLng(position.coords.latitude, position.coords.longitude);
+    let target = L.latLng(position.coords.latitude, position.coords.longitude);
     map.setView(target, 14);
     L.marker(target).addTo(map);
     output.style.display = "none";
@@ -32,19 +32,19 @@ function rit(position) {
     out.style.display = "inherit";
 }
 
-function fileit(error) {
-    switch(error.code) {
+const fileit = (error) => {
+    switch (error.code) {
         case error.PERMISSION_DENIED:
-            output.innerHTML="Permission Denied, please give permission and try again";
+            output.innerHTML = "Permission Denied, please give permission and try again";
             break;
         case error.POSITION_UNAVAILABLE:
-            output.innerHTML="Position information is unavailable please try after some time";
+            output.innerHTML = "Position information is unavailable please try after some time";
             break;
         case error.TIMEOUT:
-            output.innerHTML ="Request Timed out, please try again";
+            output.innerHTML = "Request Timed out, please try again";
             break;
         case error.UNKNOWN_ERROR:
-            output.innerHTML="Unknown Error please try again";
+            output.innerHTML = "Unknown Error please try again";
             break;
     }
 }
