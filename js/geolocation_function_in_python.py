0191b89ef72e8cbd9367405197901f1d4e63b08d
FoxSinOfGreed1729 <70275323+FoxSinOfGreed1729@users.noreply.github.com>
2021-05-28 09:51:26 +0530

    Easy, Concise and Clear geolocation function
    Simple function for geolocation written in Python using DbIpCity from the ip2geotools python Module

---
+++ js/geolocation_function_in_python.py
@@ -0,0 +1,21 @@
+#Made by FoxSinOfGreed1729 
+#Many thanks to TJ O'Connor and FossAsia
+from ip2geotools.databases.noncommercial import DbIpCity
+import socket
+
+def geolocator(adr):
+  ipadr = ""
+  if adr[0].isdigit():
+    ipadr = adr
+  else:
+    ipadr=socket.gethostbyname(hostname)
+  response=DbIpCity.get(ipadr, api_key='free')
+  ip_address = response.ip_address
+  region = response.region
+  country = response.country
+  city = response.city
+  latitude = response.latitude
+  longitude = response.longitude
+  location_data = [ip_address, region, country, city, latitude, longitude]
+  return location_data
+  
